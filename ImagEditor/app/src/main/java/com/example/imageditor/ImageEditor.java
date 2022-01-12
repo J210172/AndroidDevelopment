a31ebe612aca847da44527749db7897dc4b8b9fd
J210172 <210172@iessierradeguara.com>
2022-01-12 12:31:08 +0100

    .

---
+++ ImagEditor/app/src/main/java/com/example/imageditor/ImageEditor.java
@@ -0,0 +1,2 @@
+package com.example.imageditor;public class ImageEditor {
+}
---
+++ Pruebas/app/src/main/java/com/example/pruebas/MapActivity.java
@@ -0,0 +1,50 @@
+package org.osmdroid.sample;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.simplemap.R;
+import org.osmdroid.views.MapView;
+
+/**
+ * Bare bones osmdroid example
+ * created on 2/17/2018.
+ *
+ * @author Alex O'Ree
+ */
+
+public class MapActivity extends AppCompatActivity {
+    private MapView mapView = null;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        //TODO check permissions
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+}
